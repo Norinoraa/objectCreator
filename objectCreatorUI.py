bda03dad06f968e92870166c05794efc4d6b719a
Norinoraa <nadiapamusaka@gmail.com>
2025-10-01 14:35:13 +0700

    objectCreatorUI.py

--- objectCreatorUI.py
+++ objectCreatorUI.py
@@ -1,38 +1,35 @@
-try:
-	from PySide6 import Qtcore, Qtgui, Qtwidget
-	from shiboken6 import wrapinstance
-except :
-	from PySide2 import Qtcore, Qtgui, Qtwidget
-	from shiboken2 import wrapinstance
+from PySide6 import QtCore, QtGui, QtWidgets
+from shiboken6 import wrapInstance
 
 import maya.OpenMayaUI as omui
 import os
+import importlib
 
-ICON_PATH = os.path.abspath(os.path.join(os.path.dirname(__flie__), 'icons'))
+ICON_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'icons'))
 
-class ObjectCreatorDialog(Qtwidget.QDialog):
+class ObjectCreatorDialog(QtWidgets.QDialog):
 	def __init__(self, parent=None):
 		super().__init__(parent)
 		
 		self.resize(300, 300)
-		self.setWindowTitle('Object Creator')
+		self.setWindowTitle('Object Creator🧩')
 
-		self.main_layout = Qtwidget.QVBoxLayout()
+		self.main_layout = QtWidgets.QVBoxLayout()
 		self.setLayout(self.main_layout)
 
-		self.object_listWidget = Qtwidget.QlistWidget()
-		self.object_listWidget.setIconSize(Qtcore.QSize(50,50))
+		self.object_listWidget = QtWidgets.QListWidget()
+		self.object_listWidget.setIconSize(QtCore.QSize(60,60))
 		self.object_listWidget.setSpacing(5)
-		self.object_listWidget.setViewMode(Qtwidget.QListView.IconMode)
-		self.object_listWidget.setMovement(Qtwidget.QListView.Static)
-		self.object_listWidget.setResizeMode(Qtwidget.QListView.Adjust)
+		self.object_listWidget.setViewMode(QtWidgets.QListView.IconMode)
+		self.object_listWidget.setMovement(QtWidgets.QListView.Static)
+		self.object_listWidget.setResizeMode(QtWidgets.QListView.Adjust)
 
 		self.main_layout.addWidget(self.object_listWidget)
 
-		self.name_layout = Qtwidget.QHBoxLayout()
+		self.name_layout = QtWidgets.QHBoxLayout()
 		self.main_layout.addLayout(self.name_layout)
-		self.name_label = Qtwidget.Qlabel('Name:')
-		self.name_lineEdit = Qtwidget.QlineEdit()
+		self.name_label = QtWidgets.QLabel('Name:')
+		self.name_lineEdit = QtWidgets.QLineEdit()
 		self.name_lineEdit.setStyleSheet(
 			'''
 				QlineEdit{
@@ -48,35 +45,37 @@ class ObjectCreatorDialog(Qtwidget.QDialog):
 		self.main_layout.addWidget(self.name_label)
 		self.name_layout.addWidget(self.name_lineEdit)
 
-		self.button_layout = Qtwidget.QHBoxLayout()
+		self.button_layout = QtWidgets.QHBoxLayout()
 		self.main_layout.addLayout(self.button_layout)
-		self.create_button = Qtwidget.QPushButton('Create')
+		self.create_button = QtWidgets.QPushButton('Create✨')
 		self.create_button.setStyleSheet(
 			'''
 				QpushButton {
-					background-color: red
+					background-color: #ED378D
 				}
 			'''
 		)
-		self.cancle_button = Qtwidget.QPushButton('Cancle')
-		self.button_layout.aStetch()
+		self.cancel_button = QtWidgets.QPushButton('Cancel❌')
+		self.button_layout.addStretch()
 		self.button_layout.addWidget(self.create_button)
-		self.button_layout.addWidget(self.cancle_button)
+		self.button_layout.addWidget(self.cancel_button)
+
+		self.initIconWidget()
 
 	def initIconWidget(self):
-		objs = ['cube', 'cone', 'sphere', 'tortus']
+		objs = ['cube', 'cone', 'sphere', 'torus']
 		for obj in objs:
-			item = Qtwidget.QlistWidgetItem(obj)
-			item.setIcon(Qtgui.QIcon(os.path.join(ICON_PATH, f'{obj}.png')))
+			item = QtWidgets.QListWidgetItem(obj)
+			item.setIcon(QtGui.QIcon(os.path.join(ICON_PATH, f'{obj}.png')))
 			self.object_listWidget.addItem(item)
 
-	def run():
-		global ui
-		try:
-			ui.close()
-		except:
-			pass
+def run():
+	global ui
+	try:
+		ui.close()
+	except:
+		pass
 
-		ptr = wrapinstance(int(omui.MQtUtil.mainWindow()), Qtwidget.Qwidget)
-		ui = ObjectCreatorDialog(parent=ptr)
-		ui.show()
+	ptr = wrapInstance(int(omui.MQtUtil.mainWindow()), QtWidgets.QWidget)
+	ui = ObjectCreatorDialog(parent=ptr)
+	ui.show()
